942ec4381e7e00010fb8754e1231b564850e08a7
Alexandr <minisam2468@gmail.com>
2022-02-08 10:22:11 +0600

    atm page

--- src/main/java/com/example/ATM/System/controller/ATMController.java
+++ src/main/java/com/example/ATM/System/controller/ATMController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -17,13 +19,44 @@ public class ATMController {
         this.bankAccountRepositroy = bankAccountRepositroy;
     }
 
-    @GetMapping("/search-user")
-    public String findOneUser(@RequestParam(name = "name") String cardNumber,Model model) {
+    @GetMapping("/atm")
+    public String findOneUser(@RequestParam(name = "name") String cardNumber, Model model) {
         System.out.println(cardNumber);
         User user = bankAccountRepositroy.findByCardNumber(cardNumber);
         System.out.println(user);
         model.addAttribute("user", user);
-        return "user/user-page";
+        return "user/atm";
     }
+
+    @PostMapping("/user/login")
+    public String login(User user, Model model) {
+        System.out.println("123");
+        bankAccountRepositroy.verification(user.cardNumber, user.getPassword());
+        user = bankAccountRepositroy.findByCardNumber(user.cardNumber);
+        System.out.println("gooood");
+        model.addAttribute("user", user);
+        return "user/atm";
+    }
+
+    @GetMapping("/user/login/{cardNumber}")
+    public String login(@PathVariable("cardNumber") String cardNumber, Model model) {
+        User user = bankAccountRepositroy.findByCardNumber(cardNumber);
+        model.addAttribute("user", user);
+        System.out.println(user.toString());
+        return "user/login";
+    }
+//    @GetMapping("/admin/user-update/{id}")
+//    public String updateUserForm(@PathVariable("id") Long id, Model model) {
+//        User user = userService.findById(id);
+//        model.addAttribute("user", user);
+//        return "admin/user-update";
+//    }
+//
+//    @PostMapping("/admin/user-update")
+//    public String updateUser(User user) {
+//        userService.saveUser(user);
+//        return "redirect:/admin/users";
+//    }
+
 
 }
